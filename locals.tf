b8737fc470e6dbcde0cf2e886cca2569f675a6f2
joesabbagh1 <joe.sabbagh2001@gmail.com>
2024-07-08 22:55:30 +0300

    fix public

--- locals.tf
+++ locals.tf
@@ -37,7 +37,7 @@ locals {
     //gitignore_template = "Terraform"
     // name               = "fem-eci-terraform-product-service"
     // topics             = ["frontend-masters", "terraform"]
-    // visibility         = "private"
+    // visibility         = "public"
     // }
 
     "fem-eci-service" = {
@@ -45,7 +45,7 @@ locals {
       gitignore_template = "Go"
       name               = "fem-eci-service"
       topics             = ["frontend-masters"]
-      visibility         = "private"
+      visibility         = "public"
     }
   }
 }
