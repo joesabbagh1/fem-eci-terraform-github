0efcfaea5af4ef8ad7f7c0eb93ec7d8c16bf7830
joesabbagh1 <joe.sabbagh2001@gmail.com>
2024-07-08 23:52:35 +0300

    add product service repo

--- locals.tf
+++ locals.tf
@@ -32,13 +32,13 @@ locals {
       visibility         = "public"
     }
 
-    //"fem-eci-terraform-product-service" = {
-    // description        = "Automation for product services"
-    //gitignore_template = "Terraform"
-    // name               = "fem-eci-terraform-product-service"
-    // topics             = ["frontend-masters", "terraform"]
-    // visibility         = "public"
-    // }
+    "fem-eci-terraform-product-service" = {
+     description        = "Automation for product services"
+     gitignore_template = "Terraform"
+     name               = "fem-eci-terraform-product-service"
+     topics             = ["frontend-masters", "terraform"]
+     visibility         = "public"
+    }
 
     "fem-eci-service" = {
       description        = "Example product service"
